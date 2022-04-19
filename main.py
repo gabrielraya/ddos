224133c20da5bc805a02de5f587387f627126474
gabrielraya <gaboraya@gmail.com>
2022-04-19 19:18:11 +0200

    fixed typo on the path to read the config file

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import run_lib
 import tensorflow as tf
 
 from absl import flags, app
-from ml_collections.config_flags import config_flags
+from ml_collections import config_flags
 
-#%%
 FLAGS = flags.FLAGS
 
 # read configuration file
@@ -18,12 +17,14 @@ config_flags.DEFINE_config_file("config", None, "Training configuration", lock_c
 flags.DEFINE_string("workdir", None, "Work directory")
 flags.DEFINE_enum("mode", None, ["train", "eval"], "Running mode: train or eval")
 flags.DEFINE_string("eval_folder", "eval", "The folder name for storing evaluation results")
-flags.mark_flags_as_required(["workdir", "mode"])
+flags.mark_flags_as_required(["workdir", "config", "mode"])
+
 
 
 def main(argv):
+    # print(FLAGS.config)
+
     if FLAGS.mode == "train":
-        print("Hello")
         # Create the working directory
         tf.io.gfile.makedirs(FLAGS.workdir)
         print("File created at :", FLAGS.workdir)
@@ -37,8 +38,12 @@ def main(argv):
         logger.addHandler(handler)
         logger.setLevel('INFO')
         # Run the training pipeline
+        print("\tTraining model")
+        # run training passing configuration file parameters and working directory
         run_lib.train(FLAGS.config, FLAGS.workdir)
+
+
 if __name__ == "__main__":
     app.run(main)
 
-#%%
+
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 echo "Running code"
 
-config_dir="C:\Users\20210512\Documents\MyGithub\sgm_ood\configs\default_cifar10_config.py"
+config_dir="./configs/vp/mnist_config.py"
 workdir="./results/mnist"
 mode="train"
 
 python main.py --config=$config_dir --workdir=$workdir --mode=$mode
-
+# python main.py --config=$config_dir
 
 echo "Done"
