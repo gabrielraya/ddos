d8b6e61164987f4bb4dd1788d7fc32c1bab8fe2c
gabrielraya <gaboraya@gmail.com>
2022-04-19 19:19:33 +0200

    add decorator to read the models name

--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,35 @@
 
 
+
+_MODELS = {}
+
+
+def register_model(cls=None, *, name=None):
+    """A decorator for registering model classes."""
+
+    def _register(cls):
+        print(name)
+        if name is None:
+            local_name = cls.__name__
+        else:
+            local_name = name
+        if local_name in _MODELS:
+            raise ValueError(f'Already registered model with name: {local_name}')
+        _MODELS[local_name] = cls
+        return cls
+
+    if cls is None:
+        return _register
+    else:
+        return _register(cls)
+
+
+def get_model(name):
+    print(_MODELS)
+    return _MODELS[name]
+
+
+
 def create_model(config):
     """
     Create the score model
@@ -7,4 +37,37 @@ def create_model(config):
     :return:
     """
     model_name = config.model.name
-    score_model = get_model()
+    score_model = get_model(model_name)(config)
+    # score_model = get_model()
+
+
+def get_model_fn(model, train=False):
+    """Create a function to give the output of the score-based model.
+
+    Args:
+      model: The score model.
+      train: `True` for training and `False` for evaluation.
+
+    Returns:
+      A model function.
+    """
+
+    def model_fn(x, labels):
+        """Compute the output of the score-based model.
+
+        Args:
+          x: A mini-batch of input data.
+          labels: A mini-batch of conditioning variables for time steps. Should be interpreted differently
+            for different models.
+
+        Returns:
+          A tuple of (model output, new mutable states)
+        """
+        if not train:
+            model.eval()
+            return model(x, labels)
+        else:
+            model.train()
+            return model(x, labels)
+
+    return model_fn
